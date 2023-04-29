7840ec91a0c230bd04382b0efbe341f324170f26
Martalore97 <2lorenzanagarciamarta10@gmail.com>
2023-04-29 08:45:47 +0200

    Days and math round

--- src/app.js
+++ src/app.js
@@ -21,6 +21,13 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -32,15 +39,23 @@ function displayForecast(response) {
       `  
         
           <div class="col-2">
-            <div class="weather-forecast-date">${forecastDay.dt}</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
             <img
-              src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+              src="https://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
               alt=""
               width="42"
             />
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max">${forecastDay.temp.max} ºC</span
-              ><span class="weather-forecast-temperature-min"> ${forecastDay.temp.min} ºC</span>
+              <span class="weather-forecast-temperature-max">${Math.round(
+                forecastDay.temp.max
+              )} ºC</span
+              ><span class="weather-forecast-temperature-min"> ${Math.round(
+                forecastDay.temp.min
+              )} ºC</span>
             </div>
           </div>
             `;
